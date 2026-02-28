f899edec4c2d96be32dbae2d44b4c784380f52fb
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2026-02-27 22:19:57 -0300

    27/02/26
    comecando a fazer login, e mostrar erro!

--- JAVASCRIPT/login.js
+++ JAVASCRIPT/login.js
@@ -7,22 +7,22 @@ const exclamation = document.querySelector(".exclamation");
 const exclamation2 = document.querySelector(".exclamation2");
 const exclamation3 = document.querySelector(".exclamation3");
 
-if (!location.href == "http://127.0.0.1:5500/PAGE/login.html#") {
-formulario.addEventListener("submit", createAccount);
+if (formulario) {
+  formulario.addEventListener("submit", createAccount);
 }
 
-
+const usuario = {
+    acesso: acess.value,
+    senha: password.value,
+    nameDeUsuario: username.value,
+  };
 
 function createAccount(e) {
   e.preventDefault();
 
   let usuariosSalvos = JSON.parse(localStorage.getItem("acesso")) || [];
 
-  const usuario = {
-    acesso: acess.value,
-    senha: password.value,
-    nameDeUsuario: username.value,
-  };
+  
 
   if (
     !usuario.acesso.trim() ||
@@ -34,10 +34,31 @@ function createAccount(e) {
     exclamation3.style.display = "flex";
   } else {
     exclamation.style.display = "none";
-    exclamation.style.display = "none";
+    exclamation2.style.display = "none";
     exclamation3.style.display = "none";
     usuariosSalvos.push(usuario);
     localStorage.setItem("acesso", JSON.stringify(usuariosSalvos));
-    fomrulario.reset();
+    formulario.reset();
   }
 }
+
+// make login page
+
+const IDAcess = document.querySelector(".idpEmail");
+const passwordAcess = document.querySelector(".iptPassword");
+const btnAcess = document.querySelector('.iptSubmit')
+const form = document.querySelector('#formId')
+ 
+
+function makeLogin() {
+
+  if (IDAcess == usuario.acesso && passwordAcess == usuario.senha) {
+    alert(`Parabéns, ${usuario.nameDeUsuario}! Você fez o login`)
+  } else {
+    
+  }
+
+
+
+}
+
