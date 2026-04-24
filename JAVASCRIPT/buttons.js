0fd6d2e5f3a0895e1ba88917d051ad6d6668bb59
Matheus Luiz <142438014+MatheusLMMARCILIO@users.noreply.github.com>
2026-04-23 21:51:03 -0300

    23/04/26

---
+++ JAVASCRIPT/buttons.js
@@ -0,0 +1,13 @@
+const like = document.querySelector(".like");
+const comment = document.querySelector(".comment");
+const share = document.querySelector(".share");
+const sendMenssage = document.querySelector(".sendMenssage");
+
+like.addEventListener('click', () => {
+
+like.classList.toggle('liked')
+
+console.log('oi')
+
+})
+
